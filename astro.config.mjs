4c9fb4fdd11819d237fc82f1b054bfc2c545cc61
snapchatprimexvx <snapchatpremiumgroup639@gmail.com>
2026-09-01 23:50:37 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
+
 export default defineConfig({
   site: 'https://mausam-2.onrender.com',
   output: 'server',
